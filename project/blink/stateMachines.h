ea1f94e04a8a427d4e685be56d6201d44c3a5752
Joshua Ramos <joshita1999@gmail.com>
2020-03-14 17:29:17 -0600

    Modified demos and started project

---
+++ project/blink/stateMachines.h
@@ -0,0 +1,6 @@
+#ifdef stateMachine_included
+#define stateMachine_included
+
+void state_advance();
+
+#endif //included
--- project/blink/wdInterruptHandler.c
+++ project/blink/wdInterruptHandler.c
@@ -2,9 +2,9 @@
 #include "stateMachines.h"
 
 void
-__interrupt_vec(WDT_VECTOR) WDT(){	/* 250 interrupts/sec */
+__interrupt_vec(WDT_VECTOR) WDT() { /* 250 interrupt/sec */
   static char blink_count = 0;
-  if (++blink_count == 150) { //125
+  if (++blink_count == 125) {
     state_advance();
     blink_count = 0;
   }
